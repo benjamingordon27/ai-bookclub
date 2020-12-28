4a00eb5fb929cf9f2d4bc096b6b95e1c9720a08d
mcfarlmt <mcfarlmt@lafayette.edu>
2020-12-27 22:17:18 -0500

    Upload Lab01

---
+++ lab01/lab_01_02.py
@@ -0,0 +1,39 @@
+from math import *
+#Description : Write a program that computes and prints the 1000th prime number.
+
+#Initialize variables
+print "The following is the 1000th prime number:"
+primes = [3]
+#index number
+number = 3
+#Total quantity of primes to find
+N = 1000
+
+#Generate odd integers greater than 1
+#Check if prime list is equal to the quantity desired
+while len(primes) < (N-1):
+#Check if number is odd
+    oddFlag = number%2
+    if oddFlag:
+        #Check if number is prime
+        for prime in primes:
+            #Module against all previously captured prime numbers
+            primeFlag = number%prime
+            if not primeFlag:
+                    break
+        if primeFlag:
+            primes.append(number)
+    number = number + 1
+#Re-insert the only even entry into begining of list
+primes.insert(0,2)
+
+sum = 0
+#Log and add to each sum each number
+for number in primes:
+    sum = log(number) + sum
+#Summary Results    
+print sum    
+N = len(primes)
+print N
+print sum/N
+
